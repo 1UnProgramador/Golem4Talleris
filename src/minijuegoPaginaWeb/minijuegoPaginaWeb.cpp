884f285b28b6716b4b259e326f57b543474cb3bc
sefin <xxx>
2025-08-14 12:01:52 -0500

    incompleto

--- src/minijuegoPaginaWeb/minijuegoPaginaWeb.cpp
+++ src/minijuegoPaginaWeb/minijuegoPaginaWeb.cpp
@@ -4,16 +4,21 @@ minijuegoPaginaWeb::minijuegoPaginaWeb(Juego* juego) : Pantalla(juego){
     /* vistaCodigo.reset(sf::FloatRect(0, 0, sf::VideoMode::getDesktopMode().width / 2, sf::VideoMode::getDesktopMode().height / 2));
     vistaCodigo.reset(sf::FloatRect(sf::VideoMode::getDesktopMode().width / 2, sf::VideoMode::getDesktopMode().height / 2, sf::VideoMode::getDesktopMode().width / 2, sf::VideoMode::getDesktopMode().height / 2)); */
 
-
-    vistaCodigo.setViewport(sf::FloatRect(0.f, 0.f, 0.5f, 1.f));
+    vistaCodigo.setViewport(sf::FloatRect(0.f, 0.f, 0.5f, 0.5f));
+    vistaPanel.setViewport(sf::FloatRect(0.f, 0.5f, 0.5f, 0.5f));
     vistaPagina.setViewport(sf::FloatRect(0.5f, 0.f, 0.5f, 1.f));
 
-    mundo.setPosition(50, 50);
-    mundo.setSize(sf::Vector2f(3000, 30));
-    mundo.setFillColor(sf::Color(100, 200, 100));
-    jugador.setRadius(20);
-    jugador.setFillColor(sf::Color::Red);
-    jugador.setPosition(50, 50);
+    fondoCodigo.setPosition(0, 0);
+    fondoCodigo.setSize(sf::Vector2f(2000, 2000));
+    fondoCodigo.setFillColor(sf::Color(255, 0, 0));
+
+    fondoPanel.setPosition(0, 0);
+    fondoPanel.setSize(sf::Vector2f(2000, 2000));
+    fondoPanel.setFillColor(sf::Color(0, 255, 0));
+
+    fondoPagina.setPosition(0, 0);
+    fondoPagina.setSize(sf::Vector2f(2000, 2000));
+    fondoPagina.setFillColor(sf::Color(0, 0, 255));
 
     objetivo.fuente.loadFromFile("../../assets/textos/Bangers-Regular.ttf");
     objetivo.Texto.setString("a");
@@ -35,12 +40,15 @@ void minijuegoPaginaWeb::actualizar(){
 
 void minijuegoPaginaWeb::renderizar(sf::RenderWindow& window){
     window.setView(vistaCodigo);
-    window.draw(mundo);
-    /* window.draw(jugador); */
+    window.draw(fondoCodigo);
 
     window.setView(vistaPagina);
-    /* window.draw(mundo); */
-    window.draw(jugador);
+    window.draw(fondoPagina);
+
+    window.setView(vistaPanel);
+    window.draw(fondoPanel);
+
+    
 
     window.setView(window.getDefaultView());
    /*  window.display(); */
