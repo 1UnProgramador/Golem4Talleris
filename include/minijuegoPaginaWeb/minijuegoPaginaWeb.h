45231a4f808df033777e8907b19a4dc4bc0f45ef
1UnProgramador <elverdaderootoku@gmail.com>
2025-08-20 20:42:49 -0500

    logica(i)

--- include/minijuegoPaginaWeb/minijuegoPaginaWeb.h
+++ include/minijuegoPaginaWeb/minijuegoPaginaWeb.h
@@ -6,6 +6,7 @@
 
 class minijuegoPaginaWeb : public Pantalla {
     private:
+        sf::Font fuente;
         struct configuracionWeb {
             sf::Font fuente;
             sf::Text Texto;
@@ -14,14 +15,29 @@ class minijuegoPaginaWeb : public Pantalla {
             int fontSize;
             int layout; // 0 = título izquierda, 1 = título centrado...
         };
+        struct Bloque {
+            sf::RectangleShape rectangulo;
+            sf::Text texto;
+            bool seleccionado = false;
+            sf::Vector2f offset;
+            bool conectado = false;
+        };
+        std::vector<Bloque> bloques;
         configuracionWeb objetivo;
         sf::View vistaCodigo;
         sf::View vistaPanel;
         sf::View vistaPagina;
+        sf::Vector2f mouseEnPanel;
+        sf::Vector2f mouseEnCodigo;
+        sf::Vector2f mouseEnPagina;
         sf::RectangleShape fondoCodigo;
         sf::RectangleShape fondoPanel;
         sf::RectangleShape fondoPagina;
-        sf::Vector2u windowSize;
+        sf::RectangleShape pagina;
+        int moviemientoPanel = 0;
+        sf::Vector2i posicionMouse;
+        sf::Vector2f posicionEnVentana;
+
     public:
         minijuegoPaginaWeb(Juego* juego);
         void ManejarEvento(sf::Event evento) override;
