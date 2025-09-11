5a1829c288999f96f5989df8de49dba85142f3cb
sefin <xxx>
2025-09-11 12:06:52 -0500

    incompleto

--- src/minijuegoRedes/minijuegoRedes.cpp
+++ src/minijuegoRedes/minijuegoRedes.cpp
@@ -10,7 +10,7 @@ const std::string dirCuadros [] = {
     "../assets/minijuegoRedes/interseccionIzquierda.png",
 };
 
-std::vector<std::vector<int>> orientacionObjetivo = {
+/* std::vector<std::vector<int>> orientacionObjetivo = {
     {270, 90, 90, 90},
     {0,  180, 90, 0},
     {180, 0, 90, 0},
@@ -22,74 +22,78 @@ std::vector<std::vector<int>> orientacionDefault = {
     {0,  0, 0, 0},
     {0, 0, 0, 0},
     {0, 0, 0, 0}
-};
-/* std::vector<std::vector<int>> piezaObjetivo = {
-    {TIPO_L, TIPO_T, TIPO_I},
-    {TIPO_T, TIPO_CRUZ, TIPO_L},
-    {TIPO_I, TIPO_L, TIPO_T}
 }; */
+std::vector<std::vector<std::string>> pieza = {
+    {"tipoL", "tipoLinea", "tipoInterseccion", "tipoInterseccion", "tipoL", "tipoL", "tipoL", "tipoLinea"},
+    {"tipoLinea", "tipoL", "tipoL", "tipoLinea", "tipoL", "tipoL", "tipoInterseccion", "tipoL"},
+    {"tipoInterseccion", "tipoCruz", "tipoL", "tipoL", "tipoL", "tipoL", "tipoInterseccion", "tipoL"},
+    {"tipoInterseccion", "tipoInterseccion", "tipoInterseccion", "tipoL", "tipoLinea", "tipoL", "tipoInterseccion", "tipoInterseccion"},
+    {"tipoL", "tipoLinea", "tipoL", "tipoL", "tipoInterseccion", "tipoLinea", "tipoL", "tipoLinea"},
+    {"tipoL", "tipoL", "tipoInterseccion", "tipoLinea", "tipoLinea", "tipoL", "tipoL", "tipoL"},
+    {"tipoInterseccion", "tipoInterseccion", "tipoL", "tipoInterseccion", "tipoLinea", "tipoL", "tipoInterseccion", "tipoL"},
+    {"tipoLinea", "tipoL", "tipoLinea", "tipoInterseccion", "tipoLinea", "tipoLinea", "tipoInterseccion", "tipoL"}
+};
 
 minijuegoRedes::minijuegoRedes(Juego* juego) : Pantalla(juego){
-    int n = 1;
-    for (int i = 0; i < orientacionDefault.size(); i++)
+    
+    
+    malla.resize(pieza.size());
+    for (size_t i = 0; i < pieza.size(); i++)
     {
-        /* code */
-    }
-
-
-    for(auto& direccion : dirCuadros){
-        for (int i = 1; i <= 5; i++)
+        malla[i].resize(pieza[i].size());
+        std::vector<cuadro> fila;
+        for (size_t j = 0; j < pieza[i].size(); j++)
         {
-            cuadro cuadrito;
-            cuadrito.textura = std::make_shared<sf::Texture>();
-            cuadrito.textura->loadFromFile(direccion);
-            cuadrito.sprite.setTexture(*cuadrito.textura);
-            cuadrito.sprite.setOrigin(cuadrito.sprite.getGlobalBounds().width / 2, cuadrito.sprite.getGlobalBounds().height / 2);
-            cuadrito.sprite.setScale(3, 3);
-            /* cuadrito.sprite.setPosition(125 * n, 500); */
-            cuadros.push_back(cuadrito);
-            n++;
+            cuadro c;
+            c.tipo = pieza[i][j];
+            /* c.rotacionObjetivo = orientacionObjetivo[i][j];
+            c.rotacion = orientacionDefault[i][j]; */
+
+            c.textura = std::make_shared<sf::Texture>();
+            c.textura->loadFromFile("../assets/minijuegoRedes/"+ c.tipo +".png");
+
+            c.sprite.setTexture(*c.textura);
+            c.sprite.setOrigin(c.sprite.getGlobalBounds().width / 2, c.sprite.getGlobalBounds().height / 2);
+            c.sprite.setScale(3, 3);
+            
+            float cellW = c.sprite.getGlobalBounds().width;
+            float cellH = c.sprite.getGlobalBounds().height;
+
+            float offsetX = (sf::VideoMode::getDesktopMode().width  / 2) - (pieza[j].size() / 2.f) * cellW;
+            float offsetY = (sf::VideoMode::getDesktopMode().height / 2) - (pieza.size() / 2.f) * cellH;
+
+            c.sprite.setPosition(offsetX + j * cellW, offsetY + i * cellH);
+
+            /* c.sprite.setPosition(((sf::VideoMode::getDesktopMode().width / 2) - 2 * c.sprite.getGlobalBounds().width) + ((j % (pieza[j].size() + 1)) * c.sprite.getGlobalBounds().width), (sf::VideoMode::getDesktopMode().height / 2) - 2 * c.sprite.getGlobalBounds().height + ((i % (pieza.size() + 1))) * c.sprite.getGlobalBounds().height); */
+
+            /* c.sprite.setRotation(c.rotacion); */
+
+            malla[i][j] = c;
+
+            fila.push_back(c);
         }
-    }
-    for (int i = 15; i <= 18; i++)
-    {
-        cuadros.at(i).sprite.rotate(90);
+        malla.push_back(fila);
     }
 
-    /* float  anchoCuadros = cuadros.at(0).sprite.getGlobalBounds().width; */
-    cuadros.at(10).sprite.setPosition((sf::VideoMode::getDesktopMode().width / 2.0f) - (2.5 * cuadros.at(10).sprite.getGlobalBounds().width), (sf::VideoMode::getDesktopMode().height / 2.0f) + (2.5 * cuadros.at(10).sprite.getGlobalBounds().height));
-    cuadros.at(0).sprite.setPosition(cuadros.at(10).sprite.getPosition().x + (cuadros.at(0).sprite.getGlobalBounds().width), cuadros.at(10).sprite.getPosition().y);
-    cuadros.at(1).sprite.setPosition(cuadros.at(0).sprite.getPosition().x + cuadros.at(0).sprite.getGlobalBounds().width, cuadros.at(0).sprite.getPosition().y);
-    cuadros.at(15).sprite.setPosition(cuadros.at(1).sprite.getPosition().x + cuadros.at(1).sprite.getGlobalBounds().width, cuadros.at(1).sprite.getPosition().y);
 
-    cuadros.at(16).sprite.setPosition(cuadros.at(15).sprite.getPosition().x, cuadros.at(15).sprite.getPosition().y - cuadros.at(15).sprite.getGlobalBounds().height);
-    cuadros.at(2).sprite.setPosition(cuadros.at(1).sprite.getPosition().x, cuadros.at(1).sprite.getPosition().y - cuadros.at(1).sprite.getGlobalBounds().height);
-    cuadros.at(17).sprite.setPosition(cuadros.at(0).sprite.getPosition().x, cuadros.at(0).sprite.getPosition().y - cuadros.at(0).sprite.getGlobalBounds().height);
-    cuadros.at(3).sprite.setPosition(cuadros.at(10).sprite.getPosition().x, cuadros.at(10).sprite.getPosition().y - cuadros.at(10).sprite.getGlobalBounds().height);
-
-    cuadros.at(25).sprite.setPosition(cuadros.at(16).sprite.getPosition().x, cuadros.at(16).sprite.getPosition().y - cuadros.at(16).sprite.getGlobalBounds().height);
-    cuadros.at(11).sprite.setPosition(cuadros.at(2).sprite.getPosition().x, cuadros.at(2).sprite.getPosition().y - cuadros.at(2).sprite.getGlobalBounds().height);
-    cuadros.at(5).sprite.setPosition(cuadros.at(17).sprite.getPosition().x, cuadros.at(17).sprite.getPosition().y - cuadros.at(17).sprite.getGlobalBounds().height);
-    cuadros.at(12).sprite.setPosition(cuadros.at(3).sprite.getPosition().x, cuadros.at(3).sprite.getPosition().y - cuadros.at(3).sprite.getGlobalBounds().height);
-
-    cuadros.at(13).sprite.setPosition(cuadros.at(12).sprite.getPosition().x, cuadros.at(12).sprite.getPosition().y - cuadros.at(12).sprite.getGlobalBounds().height);
-    cuadros.at(20).sprite.setPosition(cuadros.at(5).sprite.getPosition().x, cuadros.at(5).sprite.getPosition().y - cuadros.at(5).sprite.getGlobalBounds().height);
-    cuadros.at(21).sprite.setPosition(cuadros.at(11).sprite.getPosition().x, cuadros.at(11).sprite.getPosition().y - cuadros.at(11).sprite.getGlobalBounds().height);
-    cuadros.at(14).sprite.setPosition(cuadros.at(25).sprite.getPosition().x, cuadros.at(25).sprite.getPosition().y - cuadros.at(25).sprite.getGlobalBounds().height);
 }
 
 void minijuegoRedes::ManejarEvento(sf::Event evento){
-    for (auto &cuadro : cuadros)
+    for (size_t i = 0; i < pieza.size(); i++)
     {
-        if(cuadro.sprite.getGlobalBounds().contains(posicionEnVentana)){
+        for (size_t j = 0; j < pieza[i].size(); j++)
+        {
+            if(malla[i][j].sprite.getGlobalBounds().contains(posicionEnVentana)){
             if(evento.type == sf::Event::MouseButtonPressed  && evento.mouseButton.button == sf::Mouse::Left){
-                cuadro.sprite.rotate(90);
-                cuadro.rotacion++;
+                malla[i][j].sprite.rotate(90);
+                /* malla[i][j].rotacion++; */
             } else if (evento.type == sf::Event::MouseButtonPressed  && evento.mouseButton.button == sf::Mouse::Right){
-                cuadro.sprite.rotate(-90);
-                cuadro.rotacion--;
+                malla[i][j].sprite.rotate(-90);
+                /* malla[i][j].rotacion--; */
             }
         }
+        }
+        
     }
 
 
@@ -102,7 +106,15 @@ void minijuegoRedes::actualizar(){
 }
 
 void minijuegoRedes::renderizar(sf::RenderWindow& window){
-    for(auto& cuadrito : cuadros){
-        window.draw(cuadrito.sprite);
+    for (size_t i = 0; i < pieza.size(); i++)
+    {
+        for (size_t j = 0; j < pieza[i].size(); j++)
+        {
+            window.draw(malla[i][j].sprite);
+        }
+
     }
+    /* for(auto& cuadrito : cuadros){
+        window.draw(cuadrito.sprite);
+    } */
 }
