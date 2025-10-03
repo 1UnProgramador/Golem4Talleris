0186dac91dc4b2a0ef96346867922cab92776892
1UnProgramador <elverdaderootoku@gmail.com>
2025-10-03 07:18:09 -0500

    portatiles

---
+++ Makefile/SFML/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/TextureSaver.cpp.obj.d
@@ -0,0 +1,16 @@
+src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/TextureSaver.cpp.obj: \
+ C:\SFML-2.6.1\src\SFML\Graphics\TextureSaver.cpp \
+ C:/SFML-2.6.1/src/SFML/Graphics/TextureSaver.hpp \
+ C:/SFML-2.6.1/src/SFML/Graphics/GLCheck.hpp \
+ C:/SFML-2.6.1/include/SFML/Config.hpp \
+ C:/SFML-2.6.1/src/SFML/Graphics/GLExtensions.hpp \
+ C:/SFML-2.6.1/extlibs/headers/glad/include/glad/gl.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdint.h \
+ C:/msys64/mingw64/include/stdint.h C:/msys64/mingw64/include/crtdefs.h \
+ C:/msys64/mingw64/include/corecrt.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/vadefs.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stddef.h \
+ C:/msys64/mingw64/include/stddef.h
