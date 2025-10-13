4ce0608d2871a3a8f2e3e5d4d84eaa0129d03df1
1UnProgramador <elverdaderootoku@gmail.com>
2025-10-13 14:42:20 -0500

    estable

--- src/minijuegoElectronicaYControl/minijuegoElectronicaYControl.cpp
+++ src/minijuegoElectronicaYControl/minijuegoElectronicaYControl.cpp
@@ -68,47 +68,121 @@ minijuegoElectronicaYControl::minijuegoElectronicaYControl(Juego* juego) : Panta
 
     puntero.setPosition((lineasVerticales[17].getPosition().x + movimiento / 2), puntero.getPosition().y);
 
-    sf::RectangleShape cuadrado;
-    cuadrado.setSize(sf::Vector2f(movimiento, movimiento));
-    cuadrado.setOrigin(cuadrado.getGlobalBounds().width / 2, cuadrado.getGlobalBounds().height / 2);
-    cuadrado.setFillColor(sf::Color::Blue);
-    cuadrado.setPosition(-20, -20);
-    std::cout << "cuadrado creado en: X:" << std::to_string(cuadrado.getPosition().x) << " Y: " << std::to_string(cuadrado.getPosition().y) << std::endl;
+    cubo cuadrado;
+    cuadrado.forma.setSize(sf::Vector2f(movimiento, movimiento));
+    cuadrado.forma.setOrigin(cuadrado.forma.getGlobalBounds().width / 2, cuadrado.forma.getGlobalBounds().height / 2);
+    cuadrado.forma.setFillColor(sf::Color::Blue);
+    cuadrado.forma.setPosition(-20, -20);
+    std::cout << "cuadrado creado en: X:" << std::to_string(cuadrado.forma.getPosition().x) << " Y: " << std::to_string(cuadrado.forma.getPosition().y) << std::endl;
     cubos.push_back(cuadrado);
+
+    for (int i = 0; i <= 42; i++)
+    {
+        cubo cuadrado;
+        cuadrado.forma.setSize(sf::Vector2f(movimiento, movimiento));
+        cuadrado.forma.setOrigin(cuadrado.forma.getGlobalBounds().width / 2, cuadrado.forma.getGlobalBounds().height / 2);
+        cuadrado.forma.setFillColor(sf::Color(0, 0, 0, 150));
+
+        cubosObjetivo.push_back(cuadrado);
+    }
+    for (int i = 0; i <= 9; i++){
+        cubosObjetivo[i].forma.setPosition(623.400024, 588.5 - (movimiento * i));
+    }
+    for (int i = 0; i <= 8; i++){ /*10 - 18*/
+        cubosObjetivo[i + 10].forma.setPosition(623.400024 + (movimiento * (i + 1)), cubosObjetivo[9].forma.getPosition().y);
+    }
+    for (int i = 0; i <= 8; i++){ /*19 - 27*/
+        cubosObjetivo[i + 19].forma.setPosition(cubosObjetivo[18].forma.getPosition().x, cubosObjetivo[18].forma.getPosition().y + (movimiento *(i + 1)));
+    }
+    for (int i = 0; i <= 14; i++){
+        cubosObjetivo[i + 28].forma.setPosition(cubosObjetivo[9].forma.getPosition().x, cubosObjetivo[9].forma.getPosition().y - (movimiento * (i + 1)));
+    }
+
+    /* std::cout << "cuadrado creado en: X:" << std::to_string(cubosObjetivo[0].getPosition().x) << " Y: " << std::to_string(cubosObjetivo[0].getPosition().y) << std::endl; */
+
 }
 void minijuegoElectronicaYControl::ManejarEvento(sf::Event evento){
     if (evento.type == sf::Event::KeyPressed) {
         if(evento.key.code == sf::Keyboard::Down){
-            puntero.move(0, movimiento);
-            soporte.move(0, movimiento);
-            std::cout << "puntero en: X: " << std::to_string(puntero.getPosition().x) << " Y: " << std::to_string(puntero.getPosition().y) << std::endl;
+            if(puntero.getPosition().y < 566){
+                puntero.move(0, movimiento);
+                soporte.move(0, movimiento);
+                /* std::cout << "puntero en: X: " << std::to_string(puntero.getPosition().x) << " Y: " << std::to_string(puntero.getPosition().y) << std::endl; */
+            }
         } else if(evento.key.code == sf::Keyboard::Up){
-            puntero.move(0, -movimiento);
-            soporte.move(0, -movimiento);
-            std::cout << "puntero en: X: " << std::to_string(puntero.getPosition().x) << " Y: " << std::to_string(puntero.getPosition().y) << std::endl;
+            if(puntero.getPosition().y > 216){
+                puntero.move(0, -movimiento);
+                soporte.move(0, -movimiento);
+                /* std::cout << "puntero en: X: " << std::to_string(puntero.getPosition().x) << " Y: " << std::to_string(puntero.getPosition().y) << std::endl; */
+            }
         } else if(evento.key.code == sf::Keyboard::Left){
-            puntero.move(-movimiento, 0);
-            std::cout << "puntero en: X: " << std::to_string(puntero.getPosition().x) << " Y: " << std::to_string(puntero.getPosition().y) << std::endl;
+            if(puntero.getPosition().x > 594){
+                puntero.move(-movimiento, 0);
+                /* std::cout << "puntero en: X: " << std::to_string(puntero.getPosition().x) << " Y: " << std::to_string(puntero.getPosition().y) << std::endl; */
+            }
         } else if(evento.key.code == sf::Keyboard::Right){
-            puntero.move(movimiento, 0);
-            std::cout << "puntero en: X: " << std::to_string(puntero.getPosition().x) << " Y: " << std::to_string(puntero.getPosition().y) << std::endl;
+            if(puntero.getPosition().x < 772){
+                puntero.move(movimiento, 0);
+                /* std::cout << "puntero en: X: " << std::to_string(puntero.getPosition().x) << " Y: " << std::to_string(puntero.getPosition().y) << std::endl; */
+            }
         } else if(evento.key.code == sf::Keyboard::Enter){
             size_t i = 0;
             for (auto &cuadrado : cubos){
-                if(cuadrado.getPosition() == sf::Vector2f(puntero.getPosition().x, puntero.getPosition().y + (puntero.getGlobalBounds().height / 2) + (cuadrado.getGlobalBounds().height / 2))){
+                if(cuadrado.forma.getPosition() == sf::Vector2f(puntero.getPosition().x, puntero.getPosition().y + (puntero.getGlobalBounds().height / 2) + (cuadrado.forma.getGlobalBounds().height / 2))){
                     cubos.erase(cubos.begin() + i);
                 } else {
                     if (i == cubos.size() - 1){
-                        sf::RectangleShape cuadrado;
-                        cuadrado.setSize(sf::Vector2f(movimiento, movimiento));
-                        cuadrado.setOrigin(cuadrado.getGlobalBounds().width / 2, cuadrado.getGlobalBounds().height / 2);
-                        cuadrado.setFillColor(sf::Color::Blue);
-                        cuadrado.setPosition(puntero.getPosition().x, puntero.getPosition().y + (puntero.getGlobalBounds().height / 2) + (cuadrado.getGlobalBounds().height / 2));
-                        std::cout << "cuadrado creado en: X:" << std::to_string(cuadrado.getPosition().x) << " Y: " << std::to_string(cuadrado.getPosition().y) << std::endl;
+                        cubo cuadrado;
+                        cuadrado.forma.setSize(sf::Vector2f(movimiento, movimiento));
+                        cuadrado.forma.setOrigin(cuadrado.forma.getGlobalBounds().width / 2, cuadrado.forma.getGlobalBounds().height / 2);
+                        cuadrado.forma.setFillColor(sf::Color::Blue);
+                        cuadrado.forma.setPosition(puntero.getPosition().x, puntero.getPosition().y + (puntero.getGlobalBounds().height / 2) + (cuadrado.forma.getGlobalBounds().height / 2));
+                        std::cout << "cuadrado creado en: X:" << std::to_string(cuadrado.forma.getPosition().x) << " Y: " << std::to_string(cuadrado.forma.getPosition().y) << std::endl;
                         cubos.push_back(cuadrado);
                     }
                 }
                 ++i;
+            }
+            /* bool colision = false;
+            for(int j = 0; i <= 42; i++){
+                for(auto &cuadrado : cubos){
+                    float dX = std::abs(cubosObjetivo[j].getPosition().x - cuadrado.getPosition().x);
+                    float dY = std::abs(cubosObjetivo[j].getPosition().y - cuadrado.getPosition().y);
+                    if((dX < 10) && (dY < 10)){
+                        colision = true;
+                        std::cout << "+++++" << std::endl << "+++++" << std::endl << "+++++" << std::endl << "+++++" << std::endl;
+                        break;
+                    } else {
+                        colision = false;
+                        std::cout << "-----";
+                        continue;
+                    }
+                }
+            } */
+            int z = 1;
+            bool colision = false;
+            for(auto &cuadrado : cubos){
+                for(auto &cuadradoO : cubosObjetivo){
+                    if (z == 1){
+                        break;
+                    } else {
+                        float dX = std::abs(cuadradoO.forma.getPosition().x - cuadrado.forma.getPosition().x);
+                        float dY = std::abs(cuadradoO.forma.getPosition().y - cuadrado.forma.getPosition().y);
+                        if((dX < 10) && (dY < 10)){
+                            colision = true;
+                            std::cout << "+++++" << std::endl << "+++++" << std::endl << "+++++" << std::endl << "+++++" <<     std::endl;
+                            break;
+                        } else {
+                            colision = false;
+                            std::cout << "-----";
+                            continue;
+                        }
+                    }
+                }
+                z++;
+                if(colision){
+                    continue;
+                }
             }
         }
     }
@@ -127,8 +201,11 @@ void minijuegoElectronicaYControl::renderizar(sf::RenderWindow& window){
         window.draw(linea);
     }
 
+    for(auto &cuadrado : cubosObjetivo){
+        window.draw(cuadrado.forma);
+    }
     for (auto &cuadrado : cubos){
-        window.draw(cuadrado);
+        window.draw(cuadrado.forma);
     }
 
     window.draw(impresora);
